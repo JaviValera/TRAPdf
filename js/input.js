afefeb7cf2afff1c5ccfe416f044d0369631f51f
pmmollinedo <pedro.12.pm39@gmail.com>
2017-02-03 19:56:14 +0100

    js to input on index.html

---
+++ js/input.js
@@ -0,0 +1,26 @@
+$(function() {
+  // Podemos adjuntar el evento "fileselect" a todas las entradas de archivo de la página
+  $(document).on('change', ':file', function() {
+    var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [numFiles, label]);
+  });
+
+  // Podemos ver nuestro evento "fileselect" personalizado como este
+  $(document).ready( function() {
+      $(':file').on('fileselect', function(event, numFiles, label) {
+
+          var input = $(this).parents('.input-group').find(':text'),
+              log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+          if( input.length ) {
+              input.val(log);
+          } else {
+              if( log ) alert(log);
+          }
+
+      });
+  });
+  
+});
