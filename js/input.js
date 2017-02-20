b7df8aa8229bf669f591c3d4f876e99ad61fd2b9
pmmollinedo <pedro.12.pm39@gmail.com>
2017-02-20 18:26:24 +0100

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,26 +1,19 @@
-$(function() {
-  // Podemos adjuntar el evento "fileselect" a todas las entradas de archivo de la página
-  $(document).on('change', ':file', function() {
-    var input = $(this),
-        numFiles = input.get(0).files ? input.get(0).files.length : 1,
-        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-    input.trigger('fileselect', [numFiles, label]);
-  });
+$(function () {
+    $(document).on('change', ':file', function () {
+        var input = $(this),
+            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [label]);
+    });
+    $(document).ready(function () {
+        $(':file').on('fileselect', function (event, label) {
+            var input = $(this).parents('.input-group').find(':text'),
+                log = label;
 
-  // Podemos ver nuestro evento "fileselect" personalizado como este
-  $(document).ready( function() {
-      $(':file').on('fileselect', function(event, numFiles, label) {
-
-          var input = $(this).parents('.input-group').find(':text'),
-              log = numFiles > 1 ? numFiles + ' files selected' : label;
-
-          if( input.length ) {
-              input.val(log);
-          } else {
-              if( log ) alert(log);
-          }
-
-      });
-  });
-  
+            if (input.length) {
+                input.val(log);
+            } else {
+                if (log) alert(log);
+            }
+        });
+    });
 });
